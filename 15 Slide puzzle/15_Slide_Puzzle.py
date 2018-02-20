f43e1d2356908d644b9f9c03c1bfb13337cd2cc7
Edgar Morales <edmorales94@gmail.com>
2018-02-19 22:23:36 -0500

    Added a method to create a random sequence with no repetitions

---
+++ 15 Slide puzzle/15_Slide_Puzzle.py
@@ -0,0 +1,7 @@
+from random import shuffle
+
+def generateElements():
+    elements = list(range(1,17))  # a list from 0 to 6. list() not needed in Python 2
+    shuffle(elements)
+    return elements
+
