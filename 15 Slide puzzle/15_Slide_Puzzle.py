95c0df0890cc3e67543ebbf3db8b4e05973e4ff2
Edgar Morales <edmorales94@gmail.com>
2018-02-21 18:56:01 -0500

    Added a dictionary containing the directions to take

--- 15 Slide puzzle/15_Slide_Puzzle.py
+++ 15 Slide puzzle/15_Slide_Puzzle.py
@@ -8,6 +8,7 @@ class SlidePuzzle:
     numbersForTheBoard = []
     goalBoard = ((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16))
     emptyBlockCoordinates = [0, 0]
+    direction = {"LEFT": "left", "RIGHT": "right"}
 
     """---------- generate_elements method --------------------------------------------------------------------------"""
     def generate_elements(self):
