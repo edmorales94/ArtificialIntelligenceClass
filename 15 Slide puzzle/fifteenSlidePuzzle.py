f68b8a2f66e70dfa5e75ed80f2cacc2856f11183
Edgar Morales <edmorales94@gmail.com>
2018-02-22 15:10:10 -0500

    Trying to shuffle method

--- 15 Slide puzzle/fifteenSlidePuzzle.py
+++ 15 Slide puzzle/fifteenSlidePuzzle.py
@@ -1,8 +1,8 @@
 import math
 
 
-class slidePuzzle:
-    direction = {"LEFT": "l", "RIGHT": "r", "UP":"u", "DOWN": "d"}
+class SlidePuzzle:
+    direction = {"LEFT": "l", "RIGHT": "r", "UP": "u", "DOWN": "d"}
     board = []
     path = []
     solvePuzzleUsingMethod = ""
@@ -13,7 +13,7 @@ class slidePuzzle:
         for row in range(4):
             self.board.append([])
             for col in range(4):
-                   self.board[row].append(4 * row + col + 1)
+                self.board[row].append(4 * row + col + 1)
 
     """---------- print board method ----------------------------------------"""
     def __repr__(self):
@@ -21,21 +21,18 @@ class slidePuzzle:
         for row in self.board:
             for number in row:
                 if len(str(number)) == 1:
-                   string += "  " + str(number)
+                    string += "  " + str(number)
                 elif len(str(number)) > 1:
-                   string += " " + str(number)
+                    string += " " + str(number)
             string += "\n"
         return string
-    
 
     """---------- get blank space postion method ----------------------------"""
     def get_blank_space_position(self):
         for i in range(4):
             for j in range(4):
                 if self.board[i][j] == 16:
-                    print("Black space location: ", [i,j])
-                    return [i,j]
-                
+                    return [i, j]
 
     """---------- swap elements method --------------------------------------"""
     def swap_items(self, row1, column1, row2, column2):
@@ -43,66 +40,59 @@ class slidePuzzle:
         self.board[row1][column1] = self.board[row2][column2]
         self.board[row2][column2] = temp
 
-
     """---------- get move method -------------------------------------------"""
-    def where_to_move_blank_space(self, piece_to_move):
+    def get_move(self, piece_to_move):
         blank_position = self.get_blank_space_position()
         row = blank_position[0]
         column = blank_position[1]
         if row > 0 and piece_to_move == self.board[row-1][column]:
             return self.direction["UP"]
-        elif column < 3 and piece_to_move == self.board[row][column +1]:
+        elif column < 3 and piece_to_move == self.board[row][column + 1]:
             return self.direction["RIGHT"]
         elif row < 3 and piece_to_move == self.board[row + 1][column]:
             return self.direction["DOWN"]
-        elif column > 0 and piece_to_move == self.board[row][column -1]:
+        elif column > 0 and piece_to_move == self.board[row][column - 1]:
             return self.direction["LEFT"]
-
 
     """---------- move blank space method -----------------------------------"""
     def move(self, piece_to_move):
-        move = self.where_to_move__blank_space(piece_to_move)
+        move = self.get_move(piece_to_move)
 
-        if not move is None:
+        if move is not None:
             blank_space_position = self.get_blank_space_position()
             row = blank_space_position[0]
             column = blank_space_position[1]
             if move == "u":
-                self.swap_items(row,column,row-1,column)
+                self.swap_items(row, column, row-1, column)
             elif move == "r":
-                self.swap_items(row,column,row, column+1)
+                self.swap_items(row, column, row, column+1)
             elif move == "d":
                 self.swap_items(row, column, row+1, column)
             elif move == "l":
                 self.swap_items(row, column, row, column-1)
-
 
             self.lastMove = piece_to_move
             return move
 
     """---------- is goal state method --------------------------------------"""
     def is_goal_state_reached(self):
+        goal = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
         for row in range(4):
             for column in range(4):
-                piece = self.board[row][column]
-                if piece != 16:
-                    original_row = math.floor((piece-1)/4)
-                    orignal_column = (piece-1)%4
-                    if row != original_row or column != original_column:
+                if self.board[row][column] != goal[row][column]:
                         return False
         return True
 
     """---------- get a copy of board method --------------------------------"""
     def get_a_copy_of_the_board(self):
-        copy_of_puzzle = slidePuzzle()
+        copy_of_puzzle = SlidePuzzle()
         for row in range(4):
             for column in range(4):
                 copy_of_puzzle.board[row][column] = self.board[row][column]
 
         for i in range(len(self.path)):
-            copy_of_puzzle.path.append(self.path[row])
+            copy_of_puzzle.path.append(self.path[i])
         return copy_of_puzzle
-
 
     """---------- get all allowed movements ---------------------------------"""
     def get_allowed_movements(self):
@@ -110,7 +100,7 @@ class slidePuzzle:
         for row in range(4):
             for column in range(4):
                 piece = self.board[row][column]
-                if not self.where_to_move_blank_space(piece) is None:
+                if not self.get_move(piece) is None:
                     allowed_moves.append(piece)
         return allowed_moves
 
@@ -135,14 +125,26 @@ class slidePuzzle:
 
         while len(stages) > 0:
             state = stages[0]
-            stages.pop(0)
+            stages = stages[1::]
             if state.is_goal_state_reached():
                 return state.path
-            
+            stages = stages + state.visit()
+
+    def shuffle(self):
+        self.move(12)
+    def solve(self):
+        print(self.bfs())
+
+
 """---------- main ----------------------------------------------------------"""
+
+
 def main():
-    board = slidePuzzle()
-    board.get_blank_space_position()
+    board = SlidePuzzle()
+    print(board)
+    board.shuffle()
+    print(board)
+    board.bfs()
     print(board)
 
 main()
