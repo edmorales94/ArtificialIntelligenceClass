8a31d77ae0462974ee8b6590fb26287bca3815a7
Edgar Morales <edmorales94@gmail.com>
2018-02-25 23:20:36 -0500

    Program creates a result.txt file
    Writes the elements of the puzzle in a single line, and writes the
    directions to move the white space in order to solve the puzzle.

--- SlidePuzzle/JavaVersion/SearchType.java
+++ SlidePuzzle/JavaVersion/SearchType.java
@@ -1,9 +1,32 @@
 package SlidePuzzleClasses;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SearchType {
+	Node rootBFS = null;
+	PrintWriter writerForFile = null;
+	
+	public SearchType(Node root){
+		this.rootBFS = root;
+		try {
+			writerForFile = new PrintWriter("result.txt", "UTF-8");
+			writerForFile.println("Puzzle blocks:\n");
+			for(int i = 0; i < 16; i++){
+				writerForFile.print(Integer.toString(root.puzzleBoard[i]) + " ");
+			}
+			writerForFile.println();
+		} 
+		catch (FileNotFoundException e) {
+			System.out.println("No such file exists.");
+		} 
+		catch (UnsupportedEncodingException e) {
+			System.out.println("Couldn't create file");
+		}
+	}
 	
 /**************************************************************************************************
  * This method will help us see if a node is in the visited list or not visisited list. 
@@ -38,14 +61,16 @@ public class SearchType {
 		
 /**************************************************************************************************
  * Apply the Breadth First Search method to find the path to the solution
- * @param root
+ * @param rootBFS
+ * @throws UnsupportedEncodingException 
+ * @throws FileNotFoundException 
  */
-	public void BFS(Node root){
+	public void BFS(){
 		LinkedList<Node> pathToSolution = new LinkedList<Node>();
 		Queue<Node> nodesNotVisited = new LinkedList<Node>();
 		LinkedList<Node> nodesVisited = new LinkedList<Node>();
 		
-		nodesNotVisited.add(root);
+		nodesNotVisited.add(rootBFS);
 		boolean goalFound = false;
 		
 		while(nodesNotVisited.size() > 0 && !goalFound){
@@ -65,13 +90,19 @@ public class SearchType {
 				}
 			}
 		}
+		
 		if(pathToSolution.size() > 0){
+			writerForFile.println();
+			writerForFile.println("Breadth First Search");
+			writerForFile.print("Directions to move the empty space:");
 			for(int i = pathToSolution.size()-1; i >=0; i--){
 				if(!pathToSolution.get(i).direction.equalsIgnoreCase("")){
-					System.out.print(pathToSolution.get(i).direction+" ");
+					//System.out.print(pathToSolution.get(i).direction+" ");
 					//pathToSolution.get(i).printPuzzle();
+					writerForFile.write(pathToSolution.get(i).direction+ " ");
 				}
 			}
 		}
+		writerForFile.close();
 	}
 }
