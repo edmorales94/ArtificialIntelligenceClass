4e1021bfb1a28ce73f4ac9dd7813c0a0a833700d
Edgar Morales <edmorales94@gmail.com>
2018-03-01 15:56:06 -0500

    Added comments to explain what each method does

--- SlidePuzzle/JavaVersion/Shuffle.java
+++ SlidePuzzle/JavaVersion/Shuffle.java
@@ -8,6 +8,16 @@ public class Shuffle {
 	static int lastStep = 10;
 	static ArrayList<Integer> exclude = new ArrayList<Integer>();
 	
+/*************************************************************************************************
+ * This method will return a random number from start to end inclusively, but will exclude the 
+ * numbers inside the ArrayList exclude. For example, if 2 is excluded, then 0,1, or 3 can be 
+ * return only.
+ * @param rnd
+ * @param start
+ * @param end
+ * @param exclude
+ * @return An int that's not in the excluded list
+ */
 	public static int generateRandom(Random rnd, int start, int end, ArrayList<Integer> exclude){
 		int random  = start + rnd.nextInt(end - start + 1 - exclude.size());
 		for(int ex: exclude){
@@ -19,6 +29,12 @@ public class Shuffle {
 		return random;
 	}
 	
+/*************************************************************************************************
+ * This method will go through the board array and return the index where the empty space is 
+ * located
+ * @param board
+ * @return Index of the empty space in the array
+ */
 	public static int findBlankSpace(int[] board){
 		int index = 0;
 		for (int i = 0; i < board.length; i++) {
@@ -30,6 +46,13 @@ public class Shuffle {
 		return index;
 	}
 	
+/*************************************************************************************************
+ * This method will fing the empty space position and find out all the place where it could move.
+ * If the empty espace can't go to the left then the number 3, representing the left movement, 
+ * will be place in the excluded list so that when we shuffle, we won't take into consideration
+ * the left movement
+ * @param board
+ */
 	public static void findIllegalMovements(int[] board){
 		exclude.clear();
 		emptyBlockIndex = findBlankSpace(board);
@@ -78,28 +101,43 @@ public class Shuffle {
 		}
 	}
 	
+/*************************************************************************************************
+ * This method will get the intial goal(in the goal state) and move the empty space randomly
+ * avoiding cancelling out movements. If last movement was up, the next movements to considered 
+ * will be up again(if allowed), right(if allowed), and left(if allowed). Down was excluded 
+ * because if we go down, we will cancel out the previous movement and return to the initial 
+ * location
+ * @param board
+ * @return The board shuffled 10 times
+ */
 	public static int[] shuffleBoard(int[] board){
 		Random rnd = new Random();
 		for(int i = 0; i < 10; i++){
-			findIllegalMovements(board);
-			int direction = generateRandom(rnd, 0, 3, exclude);
+			findIllegalMovements(board);//exclude the movements we shouldn't consider
+			int direction = generateRandom(rnd, 0, 3, exclude);//get a random number from 0 to 3 
+														//excluding the movements in the list
 			
-			if(direction == 0){
+			if(direction == 0){//move up if allowed
 				moveUp(board, emptyBlockIndex);
 			}
-			else if(direction == 1){
+			else if(direction == 1){//move to the right if allowed
 				moveRight(board, emptyBlockIndex);
 			}
-			else if(direction == 2){
+			else if(direction == 2){//move down if allwed
 				moveDown(board, emptyBlockIndex);
 			}
-			else if(direction == 3){
+			else if(direction == 3){//move to the left if allowed
 				moveLeft(board, emptyBlockIndex);
 			}
 		}
 		return board;
 	}
 	
+/*************************************************************************************************
+ * This method will swap the empty space with the block above it
+ * @param board
+ * @param blankBlockIndex
+ */
 	public static void moveUp(int[] board, int blankBlockIndex){
 		if(blankBlockIndex - 4 >= 0){
 			int temp = board[blankBlockIndex-4];
@@ -110,6 +148,11 @@ public class Shuffle {
 		}
 	}
 	
+/*************************************************************************************************
+ * This method will swap the empty space with the block to its right
+ * @param board
+ * @param blankBlockIndex
+ */
 	public static void moveRight(int[]board, int blankBlockIndex){
 		if(blankBlockIndex%4 < 3){
 			int temp = board[blankBlockIndex + 1];
@@ -120,6 +163,11 @@ public class Shuffle {
 		}
 	}
 	
+/*************************************************************************************************
+ * This method will swap the empty space with the block below it
+ * @param board
+ * @param blankBlockIndex
+ */
 	public static void moveDown(int [] board, int blankBlockIndex){
 		if(blankBlockIndex + 4 <= 15){
 			int temp = board[blankBlockIndex + 4];
@@ -130,6 +178,11 @@ public class Shuffle {
 		}
 	}
 	
+/*************************************************************************************************
+ * This method will swap the empty space with the block to its left
+ * @param board
+ * @param blankBlockIndex
+ */
 	public static void moveLeft(int[] board, int blankBlockIndex){
 		if(blankBlockIndex%4 > 0){
 			int temp = board[blankBlockIndex-1];
