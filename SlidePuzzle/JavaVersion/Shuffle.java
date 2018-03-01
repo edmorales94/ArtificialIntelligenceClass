67b629ffabdf7ec91026d0a30c72b2db1011f327
Edgar Morales <edmorales94@gmail.com>
2018-03-01 02:30:24 -0500

    Implemented a shuffle method that will avoid canceling out movements
    If i moved up, then I shouldn't move down because I'd be going back to
    the initial position

--- SlidePuzzle/JavaVersion/Shuffle.java
+++ SlidePuzzle/JavaVersion/Shuffle.java
@@ -1,48 +1,100 @@
 package SlidePuzzleClasses;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Shuffle {
-	static int emptyBlockIndex = 15;
+	static int emptyBlockIndex;
 	static int lastStep = 10;
+	static ArrayList<Integer> exclude = new ArrayList<Integer>();
+	
+	public static int generateRandom(Random rnd, int start, int end, ArrayList<Integer> exclude){
+		int random  = start + rnd.nextInt(end - start + 1 - exclude.size());
+		for(int ex: exclude){
+			if( random < ex){
+				break;
+			}
+			random++;
+		}
+		return random;
+	}
+	
+	public static int findBlankSpace(int[] board){
+		int index = 0;
+		for (int i = 0; i < board.length; i++) {
+			if(board[i] == 16){
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+	
+	public static void findIllegalMovements(int[] board){
+		exclude.clear();
+		emptyBlockIndex = findBlankSpace(board);
+		
+		//--------possibilities of going up----------------------------
+		if(emptyBlockIndex - 4 >= 0){//if we can go up
+			if(lastStep == 2){// if last step was down
+				exclude.add(0);//we shouldn't go up
+			}
+		}
+		if(emptyBlockIndex - 4 < 0){//if we can't go up
+			exclude.add(0);//exclude it from our choices
+		}
+		
+		//--------possibilities of going to the right -----------------
+		if(emptyBlockIndex%4 < 3){//if we can go right
+			if(lastStep == 3){//if last step was left
+				exclude.add(1);//we should go to the right
+			}
+		}
+		
+		if(emptyBlockIndex%4 == 3){//if we can't go to the right
+			exclude.add(1);//exclude it from the options
+		}
+		
+		//---------possibilities of going down ------------------------
+		if(emptyBlockIndex + 4 <= 15){//if we can go down
+			if(lastStep == 0){//but last step was up
+				exclude.add(2);//we shouldn't go up
+			}
+		}
+		
+		if(emptyBlockIndex + 4 > 15){//if we can't go down
+			exclude.add(2);//exclude it from the options
+		}
+		
+		//---------possibilities of going to the left -----------------
+		if(emptyBlockIndex%4 > 0){//if we can go left
+			if(lastStep == 1){//but last step was to the right
+				exclude.add(3);//we shouldn't go to the left
+			}
+		}
+		
+		if(emptyBlockIndex%4 == 0){// if we can't go left
+			exclude.add(3);//excluded it from the options
+		}
+	}
 	
 	public static int[] shuffleBoard(int[] board){
-		Random randomNumber = new Random();
-		int lastStep = 10;
+		Random rnd = new Random();
 		for(int i = 0; i < 10; i++){
-			int direction = randomNumber.nextInt(4);//numbers from 0 to 3
+			findIllegalMovements(board);
+			int direction = generateRandom(rnd, 0, 3, exclude);
 			
 			if(direction == 0){
-				if(lastStep != 2){
-					moveUp(board, emptyBlockIndex);
-				}
-				else{
-					moveLeft(board, emptyBlockIndex);
-				}
+				moveUp(board, emptyBlockIndex);
 			}
 			else if(direction == 1){
-				if(lastStep != 3){
-					moveRight(board, emptyBlockIndex);
-				}
-				else{
-					moveDown(board, emptyBlockIndex);
-				}
+				moveRight(board, emptyBlockIndex);
 			}
 			else if(direction == 2){
-				if(lastStep != 0){
-					moveDown(board, emptyBlockIndex);
-				}
-				else{
-					moveRight(board, emptyBlockIndex);
-				}
+				moveDown(board, emptyBlockIndex);
 			}
 			else if(direction == 3){
-				if(lastStep != 1){
-					moveLeft(board, emptyBlockIndex);
-				}
-				else{
-					moveUp(board, emptyBlockIndex);
-				}
+				moveLeft(board, emptyBlockIndex);
 			}
 		}
 		return board;
@@ -53,8 +105,8 @@ public class Shuffle {
 			int temp = board[blankBlockIndex-4];
 			board[blankBlockIndex-4] = board[blankBlockIndex];
 			board[blankBlockIndex] = temp;
-			emptyBlockIndex = blankBlockIndex - 4;
 			lastStep = 0;
+			System.out.println("Moved up");
 		}
 	}
 	
@@ -63,8 +115,8 @@ public class Shuffle {
 			int temp = board[blankBlockIndex + 1];
 			board[blankBlockIndex + 1] = board[blankBlockIndex];
 			board[blankBlockIndex] = temp;
-			emptyBlockIndex = blankBlockIndex + 1;
 			lastStep = 1;
+			System.out.println("Moved right");
 		}
 	}
 	
@@ -73,8 +125,8 @@ public class Shuffle {
 			int temp = board[blankBlockIndex + 4];
 			board[blankBlockIndex + 4] = board[blankBlockIndex];
 			board[blankBlockIndex] = temp;
-			emptyBlockIndex = blankBlockIndex + 4;
 			lastStep = 2;
+			System.out.println("Moved down");
 		}
 	}
 	
@@ -83,8 +135,8 @@ public class Shuffle {
 			int temp = board[blankBlockIndex-1];
 			board[blankBlockIndex-1] = board[blankBlockIndex];
 			board[blankBlockIndex] = temp;
-			emptyBlockIndex = blankBlockIndex - 1;
 			lastStep = 3;
+			System.out.println("Moved left");
 		}
 	}
 }
