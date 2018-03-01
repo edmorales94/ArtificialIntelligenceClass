a9f62e35a34223912639099f33367b2a59f1ab50
Edgar Morales <edmorales94@gmail.com>
2018-03-01 16:27:48 -0500

    Added comments to the BFS method and establish a limit of steps

--- SlidePuzzle/JavaVersion/SearchType.java
+++ SlidePuzzle/JavaVersion/SearchType.java
@@ -70,7 +70,7 @@ public class SearchType {
 		LinkedList<Node> nodesNotVisited = new LinkedList<Node>();//nodes to visit
 		LinkedList<Node> nodesVisited = new LinkedList<Node>();//nodes visited
 		
-		nodesNotVisited.push(rootDFS);//insert the root to the nodes to vist list
+		nodesNotVisited.push(rootDFS);//insert the root to the nodes to visit list
 		boolean goalFound = false;//variable to break the while loop
 		int steps = 0;//limit for the levels to search through
 		
@@ -85,7 +85,7 @@ public class SearchType {
 				}
 				currentNode.expandNode();//if the current node isnt the goal, create its children
 				for(int i = 0; i < currentNode.children.size(); i ++){//go through all its children
-					Node currentChild = currentNode.children.get(i);//get each one at the time
+					Node currentChild = currentNode.children.get(i);//get each one at a time
 					if(currentChild.isGoalReached()){//if the current child is the goal
 						System.out.println("Goal reached! DFS");
 						goalFound = true;//update the condition
@@ -128,41 +128,49 @@ public class SearchType {
  * @param rootBFS
  */
 	public void BFS(){
-		LinkedList<Node> pathToSolution = new LinkedList<Node>();
-		Queue<Node> nodesNotVisited = new LinkedList<Node>();
-		LinkedList<Node> nodesVisited = new LinkedList<Node>();
+		LinkedList<Node> pathToSolution = new LinkedList<Node>();//list to store the ancestor nodes
+		Queue<Node> nodesNotVisited = new LinkedList<Node>();//nodes to visit
+		LinkedList<Node> nodesVisited = new LinkedList<Node>();//nodes visited
 		
-		nodesNotVisited.add(rootBFS);
-		boolean goalFound = false;
-		int steps = 0;
-		while(nodesNotVisited.size() > 0 && !goalFound){
-			Node currentNode = nodesNotVisited.remove();
-			nodesVisited.add(currentNode);
-			
-			currentNode.expandNode();
-			for(int i = 0; i < currentNode.children.size(); i++){
-				Node currentChild = currentNode.children.get(i);
-				if(currentChild.isGoalReached()){
+		nodesNotVisited.add(rootBFS);//insert the root to the nodes to visit list
+		boolean goalFound = false;//variable to break the while loop
+		int steps = 0;//limit for the levels to search through
+		
+		while(nodesNotVisited.size() > 0 && !goalFound && steps < 1500){
+			Node currentNode = nodesNotVisited.remove();//remove the first element in the queue
+			if(currentNode.isGoalReached()){//if the current node is the goal
+				System.out.println("Goal reached! BFS");
+				goalFound = true;//update the state
+				tracePath(pathToSolution, currentNode);//give the list so we can trace back
+				break;//break the whole while loop
+			}
+			nodesVisited.add(currentNode);//since the current node is not the goal, we marked it as visited
+			currentNode.expandNode();//if the current node isnt the goal, create its children
+			for(int i = 0; i < currentNode.children.size(); i++){//go through all its children
+				Node currentChild = currentNode.children.get(i);//get each one at a time
+				if(currentChild.isGoalReached()){//if the current child is the goal
 					System.out.println("Goal reached!BFS");
-					goalFound = true;
-					tracePath(pathToSolution, currentChild);
+					goalFound = true;//update the condition
+					tracePath(pathToSolution, currentChild);//give the list so we can backtrack
 				}
+				//if the current child isn't the goal, check we haven't visited or it's not waiting
 				if(!contains(nodesVisited, currentChild)&& !contains((LinkedList<Node>) nodesNotVisited, currentChild)){
-					nodesNotVisited.add(currentChild);
+					nodesNotVisited.add(currentChild);//if it hasn't been visited or isn't waiting
+					//we added to the list of nodes to visit
 				}
 			}
-			steps++;
+			steps++;//increment the steps taken so far
 		}
 		System.out.println(steps);
 		
-		if(steps >= 30 && !goalFound){
-			writerForFile.println();
+		if(steps >= 1500 && !goalFound){//if we didn't find the goal within the steps allowed
+			writerForFile.println();//we write to the file that we needed more steps
 			writerForFile.println("Breadth First Search");
 			writerForFile.println(steps + " steps were not enough to find a solution");
 		}
 		
-		else if(pathToSolution.size() > 0 && goalFound){
-			writerForFile.println();
+		else if(pathToSolution.size() > 0 && goalFound){//if the goal was reached
+			writerForFile.println();//we write to the file the instructions to solve the puzzle
 			writerForFile.println("Breadth First Search");
 			writerForFile.println("It took: " + steps + " steps to find a solution");
 			writerForFile.print("Directions to move the empty space:");
@@ -174,8 +182,6 @@ public class SearchType {
 				}
 			}
 		}
-		
-		writerForFile.println();
 		writerForFile.println();
 	}
 }
