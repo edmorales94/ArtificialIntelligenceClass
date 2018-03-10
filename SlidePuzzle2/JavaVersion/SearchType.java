f20ed3c205e9794f3fbf6503c53ed9799d11cca8
Edgar Morales <edmorales94@gmail.com>
2018-03-09 21:30:24 -0500

    Created a new comparator for the priority queue

--- SlidePuzzle2/JavaVersion/SearchType.java
+++ SlidePuzzle2/JavaVersion/SearchType.java
@@ -132,20 +132,93 @@ public class SearchType {
 		}
 	}
 	
+	
+	/**
+	 * 
+	 */
+	public void AStarSearch(){
+		LinkedList<Node> pathToSolution = new LinkedList<Node>();//list to store the ancestor nodes
+		LinkedList<Node> nodesVisited = new LinkedList<Node>();//nodes visited
+		Comparator<Node> comparator = new Comparator<Node>(){
+			@Override
+			public int compare(Node node1, Node node2) {
+				return (int)(node1.nodeScore - node2.nodeScore);
+			}
+		};
+		PriorityQueue<Node> nodesNotVisited = new PriorityQueue<Node>(100, comparator);
+		
+		nodesNotVisited.add(starNode);//insert the root to the nodes to visit list
+		boolean goalFound = false;//variable to break the while loop
+		int maxGraphDepth = 15;
+		int currentDepth = 0;
+		int nodesGenerated = 1;//the root is the default node
+		start:{
+			while(nodesNotVisited.size() > 0 && !goalFound && currentDepth < maxGraphDepth){
+				Node currentNode = nodesNotVisited.remove();//remove the first element in the queue
+				currentDepth = currentNode.depthLevel;
+				if(currentNode.isGoalReached()){//if the current node is the goal
+					System.out.println("Goal reached! A* Search");
+					goalFound = true;//update the state
+					tracePath(pathToSolution, currentNode);//give the list so we can trace back
+					break start;//break the whole while loop
+				}
+				nodesVisited.add(currentNode);//since the current node is not the goal, we marked it as visited
+				currentNode.expandNode();//and create its children
+				for(int i = 0; i < currentNode.children.size(); i++){//go through all its children
+					Node currentChild = currentNode.children.get(i);//get each one at a time
+					//if the currentChild hasn't visited or it's not waiting
+					if(!contains(nodesVisited, currentChild)&& !contains(nodesNotVisited, currentChild)){
+						currentChild.depthLevel = currentNode.depthLevel + 1;
+						nodesGenerated++;
+						nodesNotVisited.add(currentChild);//if it hasn't been visited or isn't waiting
+						//we added to the list of nodes to visit
+					}
+				}
+			}
+		}
+		
+		if(currentDepth >= maxGraphDepth && !goalFound){//if we didn't find the goal within the steps allowed
+			System.out.println("A* Search couldn't find a solution");
+			writerForFile.println();//we write to the file that we needed more steps
+			writerForFile.println("A* Search");
+			writerForFile.println(currentDepth + " levels depth was not enough to find a solution");
+			writerForFile.println("Try running the program again to get a different puzzle board");
+		}
+		
+		else if(pathToSolution.size() > 0 && goalFound){//if the goal was reached
+			writerForFile.println();//we write to the file the instructions to solve the puzzle
+			writerForFile.println("A* Search");
+			writerForFile.println("It took: " + currentDepth + " levels depth to find a solution");
+			writerForFile.println("Nodes generated: " + nodesGenerated);
+			writerForFile.print("Directions to move the empty space:");
+			writerForFile.println();
+			for(int i = pathToSolution.size()-1; i >=0; i--){
+				if(!pathToSolution.get(i).direction.equalsIgnoreCase("")){
+					//System.out.print(pathToSolution.get(i).direction+" ");
+					//pathToSolution.get(i).printPuzzle();
+					writerForFile.write(pathToSolution.get(i).direction+ " ");
+				}
+			}
+			writerForFile.println();
+		}
+		writerForFile.close();
+	}
+	
 /**
  * AStar method
  */
-	public void AStarSearch(){
+	public void AStarSearch2(){
 		LinkedList<Node> pathToSolution = new LinkedList<Node>();
 		Set<Node> explored = new HashSet<Node>();
-		PriorityQueue<Node> notExplored = new PriorityQueue<Node>(100, new Comparator<Node>(){
-			//override compare method
-			public int compare(Node i, Node j){
-				if(i.nodeScore > j.nodeScore){return 1;}
-				else if(i.nodeScore < j.nodeScore){return -1;}
-				else{return 0;}
+		
+		Comparator<Node> comparator = new Comparator<Node>(){
+			@Override
+			public int compare(Node node1, Node node2) {
+				return (int)(node1.nodeScore - node2.nodeScore);
 			}
-		});
+		};
+		PriorityQueue<Node> notExplored = new PriorityQueue<Node>(100, comparator);
+		starNode.nodeScore = starNode.manhattanDistance(starNode.puzzleBoard);
 		notExplored.add(starNode);
 		boolean found = false;
 		int maxGraphDepth = 15;
