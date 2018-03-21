f834160a48986be17a63e75990b82d9eb8a13969
Edgar Morales <edmorales94@gmail.com>
2018-03-21 19:50:21 -0400

    This class will contain the coordinates for the board block

---
+++ tic-tac-toe/Point.java
@@ -0,0 +1,17 @@
+package tic_tac_toe;
+
+public class Point {
+	
+	//Position for the block in the board
+	public int x,y;
+	
+/*********************************************************************************************************************
+ * The constructor takes the x and y coordinates of the tic tac toe block
+ * @param x
+ * @param y
+ */
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
