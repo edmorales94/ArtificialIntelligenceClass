3ba6a225d14c69037742f07a921afacce5f15eda
Edgar Morales <edmorales94@gmail.com>
2018-03-22 15:49:22 -0400

    We created a board and allowed the user to decide who'll play first; computer or user

--- tic-tac-toe/TicTacToe.java
+++ tic-tac-toe/TicTacToe.java
@@ -1,5 +1,24 @@
 package tic_tac_toe;
 
-public class TicTacToe {
+import java.util.Random;
+import java.util.Scanner;
 
+public class TicTacToe {
+	
+	public static Random random = new Random();
+	
+	public static void main(String[] args) {
+		Board board = new Board();
+		Scanner keyboard = new Scanner(System.in);
+		board.displayBoard();
+		System.out.println("Select turn:\n#1 computer goes first(player X) or #2 user goes first(player O)");
+		
+		int choice = keyboard.nextInt();
+		
+		if(choice == 1) {
+			Point p = new Point(random.nextInt(3), random.nextInt(3));
+			board.placeAMove(p, board.player_X);
+			board.displayBoard();
+		}
+	}
 }
