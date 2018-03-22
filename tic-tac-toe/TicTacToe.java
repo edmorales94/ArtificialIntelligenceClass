a39ea5fab806065e7a6564fcf54180f75e39f694
Edgar Morales <edmorales94@gmail.com>
2018-03-22 16:06:56 -0400

    If the computer goes first, we use the minmax algorithm to make a move

--- tic-tac-toe/TicTacToe.java
+++ tic-tac-toe/TicTacToe.java
@@ -20,5 +20,25 @@ public class TicTacToe {
 			board.placeAMove(p, board.player_X);
 			board.displayBoard();
 		}
+		
+		while(!board.isGameOver()) {
+			boolean isMoveAllowed = true;
+			do {
+				if(!isMoveAllowed) {
+					System.out.println("Cell is already occupied");
+				}
+				System.out.println("Your turn: ");
+				Point userMove = new Point(keyboard.nextInt(), keyboard.nextInt());
+				isMoveAllowed = board.placeAMove(userMove, board.player_O);
+			}
+			while(!isMoveAllowed);
+			board.displayBoard();
+			if(board.isGameOver()) {
+				break;
+			}
+			
+			board.minmax(0, board.player_X);
+			System.out.println("Computer chose position: " + board.computerMove);
+		}
 	}
 }
